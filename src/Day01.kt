2dbbfb7db0c968d6c3df09ad2c8d2b01f5fe72ba
ReverendDread <andrewshilling10@gmail.com>
2021-12-02 17:06:31 -0600

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,13 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input.map(String::toInt).isLower()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: List<String>) = input.map(String::toInt).windowed(3).map(List<Int>::sum).isLower()
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+
 }
+
+fun List<Int>.isLower(): Int = windowed(2).map { if (it[0] < it[1]) 1 else 0 }.sumOf { it }
